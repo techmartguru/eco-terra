13e140d39184ea47185c8bbdc8f92826605471cd
techmartguru <roshankumar.singh@devops.local>
2019-04-28 18:46:23 +0530

    ecr code

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region = "${var.aws_region}"
+}
+
+
+######## ECR Resource #########
+resource "aws_ecr_repository" "jenkins" {
+  name = "${var.name}"
+}
+
+
+resource "aws_ecr_repository_policy" "jenkins" {
+  count      = "${var.create ? 1 : 0 }"
+
+  repository = "${var.repository}"
+  policy     = "${var.policy}"
+}
---
+++ variables.tf
@@ -0,0 +1,58 @@
+variable "aws_region" {
+   default = "us-east-1"
+   description = "aws region"
+}
+
+variable "aws_access_key" {
+    default = "AKIARS73AHPG5IRSGKOD"
+    description = "Access key"
+}
+
+variable "aws_secret_key" {
+    default = "+iSp3612TvFqXGvIK634aYxgq1hin2ni/q7waY3C"
+    description = "Secret Key"
+}
+
+
+variable "name" {
+  description = "(Required) Name of the repository"
+  type        = "string"
+}
+
+variable "create" {
+  description = "(Optional) Whether to create this resource or not"
+  default     = true
+}
+
+variable "repository" {
+  description = "(Required) Name of the repository to apply the policy"
+  type        = "string"
+}
+
+variable "policy" {
+  description = "(Optional) A valid bucket policy JSON document"
+  type        = "string"
+  default     = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Sid": "AllActionsAllow",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Action": [
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:BatchGetImage",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:PutImage",
+        "ecr:InitiateLayerUpload",
+        "ecr:UploadLayerPart",
+        "ecr:CompleteLayerUpload"
+      ]
+    }
+  ]
+}
+EOF
+}
